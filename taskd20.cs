3b8bb9ce71aebf9f42a4a408d8a662859f863543
Mahmoud Elbialy <119286962+Elbialy01@users.noreply.github.com>
2022-12-15 22:30:09 +0200

    Create taskd20.cs

---
+++ taskd20.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            course[] courses = {new course(){name="math",code=1021,grade=150 },
+            new course(){name="programing",code=1212,grade=130 },
+            new course(){name="control",code=1321,grade=130 }
+            };
+            student mahmoud = new student(12,"mahmoud","male", courses);
+            mahmoud.display(courses);
+        }
+    }
+  public  struct student
+    {
+     public int id { get;set; }
+        public string name { get;set; } 
+        public string gender { get;set;}
+        course[] courses;
+        
+       
+        public student(int _id,string _name,string _gender, course[]_courses)
+        {
+            id = _id;
+            name = _name;
+            gender = _gender;   
+            courses = _courses;
+           
+
+            
+            
+        }
+    public void display(course[] courses)
+        {
+            Console.WriteLine(name);
+            Console.WriteLine("-------------");
+            foreach (course c in courses)
+            {
+                Console.WriteLine("course: "+c.name+" code: "+c.code+" grades: "+c.grade);
+                Console.WriteLine("--------------------------------------------------------------------------------------------------");
+            }
+            
+        }
+
+    }
+    public struct course
+    {
+    
+        public int code { get; set; }
+        public string name { get; set; }    
+        public int grade { get; set; }
+    
+    }
+}
